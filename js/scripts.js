882f2d42e532161cb8a48e20f9765606e0ad4915
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-16 10:38:52 -0700

    build logic for suggester from value of survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
+// back end logic
+var suggestLang = function {
+  if (value <= 5) {
+    $("#experience").show();
+  } else if (value >=6) {
+    $("#intent").show
+  }
+   
+};
 
+
+
+//front end UI
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    
+  });
 });
