7002ec7e04d2849082f675e078f4c58385211276
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-16 13:35:31 -0700

    add working branching logic to select correct output, needs display scripts fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,30 @@
 // back end logic
 
-//this is to convert values from survey into int
-
-// var intValues = function {
-//   $("form#survey").submit(function(){
-//     
-//     var intentVal = parseInt.($("survey#intent").val());
-//     var applicationVal = parseInt.($("survey#experience").val());
-//   });
-// };
-
-
-// var selectVal = parseInt($("#experience").val());
-
-
+// var totalVal = experienceValue += intentValue += applicationValue += interestValue += levelValue +=
 
 
 //front end UI
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-    console.log('event');
     var experienceValue = parseInt($("#experience").val());
     var intentValue = parseInt($("#intent").val());
     var applicationValue = parseInt($("#application").val());
     var interestValue = parseInt($("#interest").val());
     var levelValue = parseInt($("#level").val());
-
+    var totalVal = experienceValue += intentValue += applicationValue += interestValue += levelValue;
     
-    if (totalVal <= 7) {
+    if (totalVal <= 5) {
       $("#javascript").show();
-    } else if (totalVal >= 8) {
+      $("#python").hide();
+      $("#ruby").hide();
+    } else if (totalVal >= 6) {
+      $("#javascript").hide();
       $("#python").show();
-    } else if (totalVal >= 26) {
+      $("#ruby").hide();
+    } else if (totalVal >= 11) {
+      $("#javascript").hide();
+      $("#python").hide();
       $("#ruby").show();
     }
   });
